8114bdf90df8d3bd466f4c2d2963ce8e927b243d
Ask Attilla Dagestad Strande <aska.d.s@hotmail.com>
2019-11-06 21:24:12 +0100

    starting out

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,42 @@
 import React from "react"
 
-export default () => <div>Hello World!</div>
+//Style
+import mainStyle from "../styles/main.module.scss"
+import projectSectionStyle from "../styles/projectSection.module.scss"
+
+//Componenets
+import Socialicon from "../components/Socialicon"
+import ProjectTeaser from "../components/ProjectTeaser"
+
+//Assets
+import GithubIcon from "../assets/github.svg"
+import LinkedinIcon from "../assets/linkedin.svg"
+import MailIcon from "../assets/mail.svg"
+
+const Index = () => (
+  <div>
+    <div className={mainStyle.wrapper}>
+      <div>
+        <h1 className={mainStyle.title}>askAttilla</h1>
+        <div className={mainStyle.iconWrap}>
+          <Socialicon link="#" title="Github" src={GithubIcon}></Socialicon>
+          <Socialicon link="#" title="LinkedIn" src={LinkedinIcon}></Socialicon>
+          <Socialicon link="#" title="Mail" src={MailIcon}></Socialicon>
+        </div>
+      </div>
+    </div>
+    <div className={projectSectionStyle.container}>
+      <ProjectTeaser title="Personal website" focus="JS">
+        <p className={projectSectionStyle.description}></p>
+      </ProjectTeaser>
+      <ProjectTeaser title="Slide puzzle game" focus="Python">
+        <p className={projectSectionStyle.description}></p>
+      </ProjectTeaser>
+      <ProjectTeaser title="Website for Steinhuset" focus="HTML/CSS">
+        <p className={projectSectionStyle.description}></p>
+      </ProjectTeaser>
+    </div>
+  </div>
+)
+
+export default Index
